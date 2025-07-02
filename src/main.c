819632fed3e48543ee6f66983e484900842f824a
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-07-02 12:31:56 -0300

    suporte básico para o formato .obj

--- src/main.c
+++ src/main.c
@@ -29,97 +29,17 @@ int main(int argc, char* argv[]) {
     ColorHSV color1 = {345, 0.9, 1};
     ColorHSV color2 = {0, 0, 0.75};
 
-    Mesh obj1 = createMesh(
-        (Point[]) {
-            {  0.00,  3.00,  2.00 },  
-            {  1.90,  3.00,  0.62 },  
-            {  1.18,  3.00, -1.62 },  
-            { -1.18,  3.00, -1.62 },  
-            { -1.90,  3.00,  0.62 },  
-            {  0.00,  1.75,  3.50 },  
-            {  3.33,  1.75,  1.08 },  
-            {  2.06,  1.75, -2.83 },  
-            { -2.06,  1.75, -2.83 },  
-            { -3.33,  1.75,  1.08 },  
-            {  0.00, -2.50,  0.00 }   
-        },
-        (Triangle[]) {
-            {{0, 1, 2}, color1},
-            {{0, 2, 3}, color1},
-            {{0, 3, 4}, color1},
+    MeshList mesh_list;
+    initMeshList(&mesh_list);
 
-            {{0, 5, 6}, color1},
-            {{0, 6, 1}, color1},
-
-            {{1, 6, 7}, color1},
-            {{1, 7, 2}, color1},
-
-            {{2, 7, 8}, color1},
-            {{2, 8, 3}, color1},
-
-            {{3, 8, 9}, color1},
-            {{3, 9, 4}, color1},
-
-            {{4, 9, 5}, color1},
-            {{4, 5, 0}, color1},
-            
-            {{9, 10, 5}, color1},
-
-            {{5, 10, 6}, color1},
-
-            {{6, 10, 7}, color1},
-
-            {{7, 10, 8}, color1},
-
-            {{8, 10, 9}, color1},
-        },
-        11, 18, (Material) {0.4, 0.8});
-
-    Mesh obj2 = createMesh(
-        (Point[]) {
-            {3 * 0 - 8, 3 * 0, 3 * 0 - 8},         
-            {3 * 1 - 8, 3 * 0, 3 * 0 - 8},          
-            {3 * 1 - 8, 3 * 0, 3 * 1 - 8},          
-            {3 * 0 - 8, 3 * 0, 3 * 1 - 8},          
-            {3 * 0 - 8, 3 * 1, 3 * 0 - 8},          
-            {3 * 1 - 8, 3 * 1, 3 * 0 - 8},          
-            {3 * 1 - 8, 3 * 1, 3 * 1 - 8},          
-            {3 * 0 - 8, 3 * 1, 3 * 1 - 8},          
-            {3 * 0.5 - 8, 3 * 1.5, 3 * 0.5 - 8}     
-        },
-        (Triangle[]) {
-            {{0, 2, 1}, color2},
-            {{0, 3, 2}, color2},
-
-            {{0, 5, 1}, color2},
-            {{0, 4, 5}, color2},
-
-            {{1, 6, 2}, color2},
-            {{1, 5, 6}, color2},
-
-            {{2, 7, 3}, color2},
-            {{2, 6, 7}, color2},
-
-            {{3, 4, 0}, color2},
-            {{3, 7, 4}, color2},
-
-            {{4, 6, 5}, color2},
-            {{4, 7, 6}, color2},
-
-            {{4, 8, 5}, color2},
-            {{5, 8, 6}, color2},
-            {{6, 8, 7}, color2},
-            {{7, 8, 4}, color2}
-        },
-        9, 16, (Material) {0.4, 0.8}
-    );
-        
-    Mesh *meshes[] = { &obj1, &obj2 };
+    for (int i = 0; i < 7; i++) {
+        loadMesh("assets/diamond.obj", &mesh_list, NULL, 0.75, (Point) {8 * i, 0, 0}, &(ColorHSV) {360 / (i + 1), 0.9, 1});
+    }
 
     LightSource light = { {5, 7, 3}, 1.0f, 0.3f, 0.001f, 0.001f, 1.0f };
 
     Camera camera;
-    initCamera(&camera, renderer, window, &(Point) {10, 5, 0}, &(Point) {0, 0, 0}, &(Vector) {0, 1, 0});
+    initCamera(&camera, renderer, window, &(Point) {10, 5, 5}, &(Point) {0, 0, 0}, &(Vector) {0, 1, 0});
 
     const Uint8 *state;
 
@@ -133,12 +53,13 @@ int main(int argc, char* argv[]) {
     bool relative_mouse_mode = true;
     SDL_SetWindowRelativeMouseMode(window, true);
 
-    float color_counter = 0;
+    float counter = 0;
 
     while (running) {
         Uint64 current_time = SDL_GetTicks();
         float delta_time = (current_time - last_time) / 1000.0f;
         last_time = current_time;
+        counter += 1 * delta_time;
 
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
@@ -196,19 +117,26 @@ int main(int argc, char* argv[]) {
             rotateCamera(&camera, -dy, dx);
         }
 
-        // obj1.angle_y += OBJECT_ROTAION_SPEED * delta_time;
-        // obj2.angle_y -= OBJECT_ROTAION_SPEED * delta_time;
+        for (int i = 0; i < mesh_list.num_meshes; i++) {
+            Mesh *mesh = &mesh_list.meshes[i];
+            mesh->angle_y -= OBJECT_ROTAION_SPEED * delta_time;
+
+            float angle = ((2.0f * PI) / ((float)mesh_list.num_meshes / (float)(i + 1))) + counter;
+
+            mesh->pos.x = sinf(angle) * 8;
+            mesh->pos.z = cosf(angle) * 8;
+        }
 
         light.pos.x = camera.pos.x;
         light.pos.y = camera.pos.y;
         light.pos.z = camera.pos.z;
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        renderObjects(&camera, meshes, 2, &light);
+        renderObjects(&camera, &mesh_list, &light);
 
         SDL_RenderPresent(renderer);
     }
--- src/modules/mesh.c
+++ src/modules/mesh.c
@@ -1,5 +1,175 @@
 #include "mesh.h"
 
-Mesh createMesh(Point *vertices, Triangle *faces, int num_vertices, int num_faces, Material material) {
-    return (Mesh) { vertices, num_vertices, faces, num_faces, 0, 0, 0, material };
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+Mesh createMesh(Point position, Point *vertices, Triangle *faces, int num_vertices, int num_faces, Material material) {
+    return (Mesh) { position, vertices, num_vertices, faces, num_faces, 0, 0, 0, material };
+}
+
+void readVertice(Point **p, float size, const Point *pos) {
+    Point *point = malloc(sizeof(Point));
+    if (point == NULL) {
+        printf("Error allocating memory for vertice\n");
+        *p = NULL;
+        return;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        char *token = strtok(NULL, " ");
+        if (token != NULL) {
+            point->data[i] = atof(token) * size;
+        } else {
+            free(point);
+            *p = NULL;
+            printf("Error reading vertice\n");
+            return;
+        }
+    }
+
+    *p = point;
+}
+
+void readFace(Triangle **a, Triangle **b, const ColorHSV *color) {
+    Triangle *face1 = malloc(sizeof(Triangle));
+    Triangle *face2 = malloc(sizeof(Triangle));
+
+    if (!face1 || !face2) {
+        printf("Error allocating memory for face\n");
+        return;
+    }
+
+    face1->color = *color;
+    face2->color = *color;
+
+    char *data[4];
+    char *token;
+
+    for (int i = 0; i < 4; i++) {
+        token = strtok(NULL, " ");
+        if (token != NULL) {
+            data[i] = strdup(token);
+        } else {
+            data[i] = NULL;
+        }
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        if (data[i]) {
+            char *vstr = strtok(data[i], "/");
+
+            int value = vstr ? atoi(vstr) - 1 : -1;
+            if (i < 3) face1->vertices[i] = value;
+            if (i > 0) face2->vertices[i - 1] = value;
+
+            free(data[i]);
+        } else {
+            if (i < 3) {
+                *a = NULL;
+                *b = NULL;
+                free(face1);
+                free(face2);
+                printf("Error reading face\n");
+                return;
+            }
+        }
+    }
+
+    *a = face1;
+    if (data[3]) {
+        face2->vertices[0] = face1->vertices[0];
+        *b = face2;
+    } else {
+        *b = NULL;
+        free(face2);
+    }
+}
+
+void loadMesh(const char *filename, MeshList *mesh_list, Mesh **mesh, float size, Point pos, const ColorHSV *color) {
+    FILE *file;
+    char line[256];
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return;
+    }
+
+    Point *vertices = NULL;
+    Triangle *faces = NULL;
+    int num_vertices = 0;
+    int num_faces = 0;
+    int vertice_capacity = 0;
+    int face_capacity = 0;
+
+    while (fgets(line, sizeof(line), file)) {
+        char *token = strtok(line, " ");
+        
+        if (token != NULL) {
+            if (strcmp(token, "v") == 0) {
+                if (num_vertices >= vertice_capacity) {
+                    vertice_capacity = vertice_capacity ? vertice_capacity * 2 : 10;
+                    vertices = realloc(vertices, vertice_capacity * sizeof(Point));
+                    if (vertices == NULL) {
+                        perror("Error allocating memory");
+                        return;
+                    }
+                }
+
+
+                Point *p = NULL;
+                readVertice(&p, size, &pos);
+                if (p) {
+                    vertices[num_vertices++] = *p;
+                    free(p);
+                } else {
+                    return;
+                }
+            } else if (strcmp(token, "f") == 0) {
+                if (num_faces >= face_capacity) {
+                    face_capacity = face_capacity ? face_capacity * 2 : 10;
+                    faces = realloc(faces, face_capacity * sizeof(Triangle));
+                    if (faces == NULL) {
+                        perror("Error allocating memory");
+                        return;
+                    }
+                }
+
+                Triangle *face1 = NULL, *face2 = NULL;
+                readFace(&face1, &face2, color);
+
+                if (face1) {
+                    faces[num_faces++] = *face1;
+                    free(face1);
+                } else {
+                    return;
+                }
+                 
+                if (face2) {
+                    faces[num_faces++] = *face2;
+                    free(face2);
+                }
+            }
+        }
+    }
+
+    fclose(file);
+        
+    if (mesh_list->num_meshes >= mesh_list->mesh_capacity) {
+        mesh_list->mesh_capacity = mesh_list->mesh_capacity ? mesh_list->mesh_capacity * 2 : 10;
+        mesh_list->meshes = realloc(mesh_list->meshes, mesh_list->mesh_capacity * sizeof(Mesh));
+        if (mesh_list->meshes == NULL) {
+            perror("Error allocating memory");
+            return;
+        }
+    }
+    mesh_list->meshes[mesh_list->num_meshes++] = createMesh(pos, vertices, faces, num_vertices, num_faces, (Material) { 0.4f, 0.8f });
+    if (mesh) *mesh = &mesh_list->meshes[mesh_list->num_meshes - 1];
+}
+
+void initMeshList(MeshList *mesh_list) {
+    mesh_list->mesh_capacity = 0;
+    mesh_list->num_meshes = 0;
+    mesh_list->meshes = NULL;
 }
